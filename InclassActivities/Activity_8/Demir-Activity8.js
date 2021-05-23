67b57c43bacd8fc19b19d1d09bdb7057a1cb0265
SNN <snndmrprx@gmail.com>
2021-05-23 19:38:10 +0300

    update

--- InclassActivities/Activity_8/Demir-Activity8.js
+++ InclassActivities/Activity_8/Demir-Activity8.js
@@ -10,19 +10,21 @@ var $ = function (id) {
 window.onload = function () {
     move();
 
-    $("enemy_button").onmousedown = clicked;
+    $("enemy_button").onclick = clicked;
     $("enemy_button").addEventListener("mouseover", () => setTimeout(move, delay));
 };
 
 var move = function () {
+    var button = $("enemy_button");
+
     game_size = window.getComputedStyle($("game"));
-    enemy_button_size = window.getComputedStyle($("enemy_button"));
+    enemy_button_size = window.getComputedStyle(button);
 
     max_width = parseInt(game_size.width, 10) - parseInt(enemy_button_size.width, 10);
     max_height = parseInt(game_size.height, 10) - parseInt(enemy_button_size.height, 10);
 
-    enemy_button.style.marginTop = Math.random() * max_height + "px";
-    enemy_button.style.marginLeft = Math.random() * max_width + "px";
+    button.style.marginTop = Math.random() * max_height + "px";
+    button.style.marginLeft = Math.random() * max_width + "px";
 };
 
 var clicked = function () {
@@ -32,14 +34,12 @@ var clicked = function () {
 
     if (clicked_counter == 3) {
         level += 1;
-        
         clicked_counter = 0;
         delay -= delay == 0 ? 0 : 100;
 
-        alert("(⌐■_■)--︻デ═一  -  Level Up!");
+        alert("Level Up!");
 
         $("clicked_text").innerHTML = "To Level Up: " + clicked_counter + "/3";
         $("level_text").innerHTML = "Level " + level + " (Delay: " + delay + "ms.)";
-        $("enemy_button").style.transition = delay + "ms";
     }
 };
