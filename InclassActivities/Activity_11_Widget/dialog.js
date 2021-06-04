4130f6c7d164f9c5895afb71ec938a74032c463d
SNN <snndmrprx@gmail.com>
2021-06-04 03:20:13 +0300

    Activity 11 init

---
+++ InclassActivities/Activity_11_Widget/dialog.js
@@ -0,0 +1,14 @@
+$(function () {
+    $("#dialog").dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            OK: function () {
+                $(this).dialog("close");
+            },
+        },
+    });
+    $("#policies").click(function () {
+        $("#dialog").dialog("open");
+    });
+});
